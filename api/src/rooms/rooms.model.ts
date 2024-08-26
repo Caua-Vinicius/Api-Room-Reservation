1727ec36d7d46e04803813b768bca202bf4daf9c
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-08-26 19:58:31 -0300

    creating an interface for Room

--- api/src/rooms/rooms.model.ts
+++ api/src/rooms/rooms.model.ts
@@ -1,6 +1,13 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export interface RoomInterface {
+  name: string;
+  capacity: number;
+  resourses: string;
+  location: string;
+}
+
 @Schema({ timestamps: true })
 export class Rooms extends Document {
   @Prop()
