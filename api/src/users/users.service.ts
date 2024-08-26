cdf14659fd309d7eb02013cc7edfb7b261f50de2
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-08-26 19:51:12 -0300

    Adding method findOneById for fetch user

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -11,4 +11,14 @@ export class UserService {
     const users = await this.userModel.find();
     return users;
   }
+
+  async findOneById(id: string): Promise<Users> {
+    try {
+      const user = this.userModel.findById(id);
+      return user;
+    } catch (error) {
+      console.error('Error finding user:', error);
+      return null;
+    }
+  }
 }
