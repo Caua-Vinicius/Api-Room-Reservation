3202acb96e94c40a2cb48d26ec74725aae58e05c
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-04-30 19:48:56 -0300

    docs: add swagger docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rooms Reservations API')
+    .setDescription('API para gerenciar gatos')
+    .setVersion('1.0')
+    .build();
+
   app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
