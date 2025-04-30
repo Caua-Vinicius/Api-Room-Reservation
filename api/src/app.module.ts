d100eab7c02e375fdec90b300bcaedcb46ad221a
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-04-30 19:56:31 -0300

    feat: add authModule to appModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { RoomsModule } from './rooms/rooms.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
