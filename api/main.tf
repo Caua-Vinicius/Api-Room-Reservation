13711697e0b78424db660f8d469f49a9b95e3178
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-05-21 22:11:25 -0300

    feat: add AWS provider and resources for app server and SSH security group

---
+++ api/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+  key_name      = "meu-par-de-chaves"
+
+  tags = {
+    Name = "MeuApp"
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Liberar SSH"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
